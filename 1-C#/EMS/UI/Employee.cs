0c8a158a48cd2c5cace0f364d6cb865d531d8261
Pushpinder <pushpinder.kaur@revature.com>
2022-04-13 13:26:25 -0400

    C# OOP app start

---
+++ 1-C#/EMS/UI/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI
+{
+    internal class Employee
+    {
+        // members -> variables, constants, methods, events, constructors etc......
+        //variable => State of the object
+        public string firstName="Joe", lastName="Dow", id="101";
+        public const string planet = "Earth";
+
+        //Methods=>Behaviour
+        public void DoTask(string firstName, string lastName, string id, float hours)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.id = id;
+            Console.WriteLine($"{firstName} {lastName} writes program in C# for {hours} hours over weekdays");
+        }
+    }
+}
---
+++ 1-C#/EMS/UI/Program.cs
@@ -0,0 +1,22 @@
+﻿// Let's call Employee
+using UI;
+
+Employee emp = new Employee();
+//emp.firstName = "Yunus";
+//emp.lastName = "Khan";
+
+//Employee.planet = "Mars"; // const can never be changed
+Console.Write("Please enter first name of the employee ");
+var firstName=Console.ReadLine();
+Console.Write("Please enter last name of the employee ");
+var lastName = Console.ReadLine();
+Console.Write("Please enter id of the employee ");
+var id = Console.ReadLine();
+Console.Write("Please submit working hours of the employee ");
+var hours= float.Parse(Console.ReadLine());
+
+emp.DoTask(firstName,lastName,id,hours);
+
+//Console.WriteLine($"Employee Id - {emp.id}\nEmployee Name - {emp.firstName} {emp.lastName}\nPlanet - {Employee.planet}");
+
+
