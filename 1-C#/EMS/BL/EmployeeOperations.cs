6f3e2374d52991fde5d9a5d238817839813727d6
Pushpinder <pushpinder.kaur@revature.com>
2022-04-13 16:06:34 -0400

    Inheritance and access specifiers

---
+++ 1-C#/EMS/BL/EmployeeOperations.cs
@@ -0,0 +1,12 @@
+﻿using UI;
+namespace BL
+{
+    public class EmployeeOperations :Employee
+    {
+        EmployeeOperations emp= new EmployeeOperations();
+        public void CheckAccess()
+        {
+            
+        }
+    }
+}
--- 1-C#/EMS/UI/Employee.cs
+++ 1-C#/EMS/UI/Employee.cs
@@ -6,12 +6,18 @@ using System.Threading.Tasks;
 
 namespace UI
 {
-    internal class Employee
+    enum Authority
+    {
+        Hire, Fire, Delegate
+    }
+    // parent/super/Base
+    public class Employee
     {
         // members -> variables, constants, methods, events, constructors etc......
         //variable => State of the object
-        public string firstName="Joe", lastName="Dow", id="101";
+        protected string firstName = "Joe", lastName = "Dow", id = "101";
         public const string planet = "Earth";
+        protected int age;
 
         //Methods=>Behaviour
         public void DoTask(string firstName, string lastName, string id, float hours)
@@ -22,4 +28,19 @@ namespace UI
             Console.WriteLine($"{firstName} {lastName} writes program in C# for {hours} hours over weekdays");
         }
     }
+    //child/sub/derived class 
+    class Manager : Employee
+    {
+       public Authority authority;
+       public string GetDetails(string firstName, string lastName, string id, int age)
+        {
+            base.firstName = firstName;
+            base.lastName = lastName;
+            base.age = age;
+            base.id = id;
+            authority = Authority.Hire;
+           return $"Name - {firstName} {lastName}\nAge - {age}\nEmployee id - {id}";
+        }
+    }
+    
 }
