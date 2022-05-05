34d31f3b369dc2f4e6123cdc4b591efba38e5888
Pushpinder <pushpinder.kaur@revature.com>
2022-05-05 16:24:00 -0400

    Async and await basic example

---
+++ 3-Services/MoreProgramsCSharp/MoreProgramsCSharp/LearnAsyncPrograms.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoreProgramsCSharp
+{
+    internal class LearnAsyncPrograms
+    {
+        public void LongMethod()
+        {
+            Console.WriteLine("-------Starting Long method-------");
+            Thread.Sleep(5000);//this is going to pause the execution by 5 secs, it takes ms as the input
+            Console.WriteLine("---------long method execution finish-------");
+        }
+        // --async means the method is asynchronous it might be having 1 or more awaits
+        public async void CallLongMethod()
+        {
+            Console.WriteLine("--inside CallLongMethod--");
+            //--await keyword ensure that where we have to wait for the intensive computation method and it will release the main thread
+            //--Task.Run  is used to create a new thread to run this method, but if this method is async as well then we need not to use Task.Run
+            //--Action is a delegate of type void
+            await Task.Run(new Action(LongMethod));
+            Console.WriteLine("--CallLongMethod finshed exec---");
+        }
+    }
+}
---
+++ 3-Services/MoreProgramsCSharp/MoreProgramsCSharp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MoreProgramsCSharp;
+
+Console.WriteLine("************Main starting**************");
+LearnAsyncPrograms obj = new LearnAsyncPrograms();
+obj.CallLongMethod();
+Console.WriteLine("***************Main End******************");
+Console.Read();
+
