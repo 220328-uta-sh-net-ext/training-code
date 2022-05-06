a1e2551191ba1039de9c4125a5fff5924265baaf
Pushpinder <pushpinder.kaur@revature.com>
2022-05-06 17:27:38 -0400

    Consume API using HttpClient

---
+++ 3-Services/ConsolePokemonApi/Pokemon.cs
@@ -0,0 +1,11 @@
+namespace ConsolePokemonApi
+{
+    public class Pokemon
+    {
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public int Attack { get; set; }
+    public int Defense { get; set; }
+    public int Health { get; set; }   
+    }
+}
---
+++ 3-Services/ConsolePokemonApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsolePokemonApi;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+using var client = new HttpClient();
+
+client.BaseAddress = new Uri("http://localhost:5185/api/pokemon");
+try{
+var response=client.GetAsync("pokemon");
+response.Wait();
+
+var result = response.Result;
+
+if(result.IsSuccessStatusCode){
+    var readTask=result.Content.ReadAsAsync<List<Pokemon>>();
+    readTask.Wait();
+
+    var pokemons = readTask.Result;
+    foreach (var p in pokemons)
+    {
+        Console.WriteLine(p.Name);
+    }
+    
+}
+}
+catch(System.Net.Http.HttpRequestException ex){
+    Console.WriteLine(ex.Message);
+}
+catch(Exception ex){
+    Console.WriteLine(ex.Message);
+}
