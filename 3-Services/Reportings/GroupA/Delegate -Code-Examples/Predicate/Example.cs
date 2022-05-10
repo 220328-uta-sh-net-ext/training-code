9d5746cc0f805c3d8a7f65899922186109d423ef
Daniel Oszczapinski <dannyoszczapinski@gmail.com>
2022-05-10 17:58:40 -0500

    Added Delegate, Anonymous, Lambda Notes and Code Examples

---
+++ 3-Services/Reportings/GroupA/Delegate -Code-Examples/Predicate/Example.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Predicate
+{
+    internal class Example
+    {
+        static bool IsUpperCase(string str)
+        {
+            return str.Equals(str.ToUpper());
+        }
+
+        static void Main(string[] args)
+        {
+            Predicate<string> isUpper = IsUpperCase;
+
+            bool result = isUpper("hello world!!");
+
+            Console.WriteLine(result);
+        }
+    }
+}
