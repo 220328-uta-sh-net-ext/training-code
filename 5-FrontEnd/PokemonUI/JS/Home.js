9d56e69454a160744c1251fa0605792926825aa0
Pushpinder <pushpinder.kaur@revature.com>
2022-05-20 12:15:33 -0400

    Functions in JS

--- 5-FrontEnd/PokemonUI/JS/Home.js
+++ 5-FrontEnd/PokemonUI/JS/Home.js
@@ -1,1 +1,32 @@
-console.log("JS working");
+//window.onload = BasicFunction();
+function basicFunction(){
+    console.log("Calling named function"); 
+};// named function*/
+
+// anonymous function - single use only and they are created and called just once 
+var anonymous= function(){
+    console.log("Calling anonymous function"); 
+};
+
+//Arrow Notation
+var arrowFunction= ()=>console.log("calling arrow function");
+
+//Arrow function, IIFE
+(()=>console.log('IIFE function called'))(); 
+
+//Prototype function - used for inheritance via functions, its like object
+function prototypeFunction(){
+    prototypeFunction.prototype.Name="something";
+    prototypeFunction.prototype.Id="Some Id";
+    console.log(prototypeFunction.prototype);
+}
+
+function greeting(name){
+    console.log("Hello "+name);
+}
+
+function Input(greeting){
+    debugger;
+    var name=prompt('Please enter your name ');
+    greeting(name);
+}
