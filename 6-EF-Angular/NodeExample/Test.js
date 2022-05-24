558a306672bacf35d31d3999199428f7f299ed0a
bryanserfozo <bryan.serfozo@revature.net>
2022-05-24 12:31:05 -0400

    Notes from Morning 5/24

---
+++ 6-EF-Angular/NodeExample/Test.js
@@ -0,0 +1,1 @@
+console.log("Test is running");
---
+++ 6-EF-Angular/NodeExample/import.js
@@ -0,0 +1,9 @@
+let greeting = require('./helloNode.js');
+
+console.log(greeting('Bryan'));
+
+// npm init creates package.json
+// npm install will install any associated packages listed in package.json
+// npm install packagename will install the specific package
+// npm install -g packagename: installs a package globally
+// npm install -D packagename: install a package as a dev dependencies
