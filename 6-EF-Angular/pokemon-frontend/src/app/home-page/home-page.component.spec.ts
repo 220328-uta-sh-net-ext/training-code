f3d256f67ed23ce9e32155068748b56d90efc764
bryanserfozo <bryan.serfozo@revature.net>
2022-05-26 14:58:37 -0400

    Remainder of notes for 5/26

--- 6-EF-Angular/pokemon-frontend/src/app/home-page/home-page.component.spec.ts
+++ 6-EF-Angular/pokemon-frontend/src/app/home-page/home-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { HomePageComponent } from './home-page.component';
 
@@ -22,4 +23,13 @@ describe('HomePageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // We can create a new test by using the it keyword and then describing a callback function to test
+  // The simplest sort of test we can make
+
+  it('should create an h1 equal to "Welcome to our Pokemon Website"', () =>{
+      const h1 = fixture.debugElement.query(By.css('h1')).nativeElement;
+
+      expect(h1.innerHTML).toBe("Welcome To Our Pokemon Website");
+  })
 });
